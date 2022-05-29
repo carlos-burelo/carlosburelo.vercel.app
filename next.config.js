aa51e56d547b32e6e5ee28e579e3abf879f22051
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-05-29 16:48:52 -0500

    :test_tube: tests(logs): remove logs from next config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 const { join } = require('path')
-const { readdirSync } = require('fs')
+
 /** @type {import('next').NextConfig} */
-
-// logs
-const isDev = process.env.NODE_ENV !== 'production'
-console.log('##############################################')
-console.log('IS_DEV: ', isDev)
-console.log('NODE_ENV: ', process.env.NODE_ENV)
-console.log(
-  'SHIKI_PATH: ',
-  isDev ? join(process.cwd(), 'public/assets') : join(process.cwd(), 'assets')
-)
-// read dir in production
-// console.log(
-//   'reading assests path [assets]:',
-//   readdirSync(join(process.cwd(), 'assets'))
-// )
-console.log(
-  'reading assests path [pubic/assets]:',
-  readdirSync(join(process.cwd(), 'public/assets'))
-)
-console.log('##############################################')
-
 const nextConfig = {
   swcMinify: true,
   sassOptions: {
