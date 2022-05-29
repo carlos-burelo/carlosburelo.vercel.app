b2ad79101fda51f0aa66f29be4538b61af3a27bf
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-05-29 17:04:21 -0500

    :bug: fix(OpenGraph): fix open-graph generator in serverless function

--- src/pages/api/open-graph.ts
+++ src/pages/api/open-graph.ts
@@ -1,11 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import playwright from "playwright-aws-lambda"
+import chromium from 'chrome-aws-lambda'
+import playwright from 'playwright-core'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const browser = await playwright.launchChromium()
+  const browser = await playwright.chromium.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  })
   const page = await browser.newPage({
     viewport: {
       width: 1200,
@@ -20,6 +25,6 @@ export default async function handler(
   const data = await page.screenshot({ type: 'jpeg' })
   await browser.close()
   res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate')
-  res.setHeader('Content-Type', 'image/png')
+  res.setHeader('Content-Type', 'image/jpeg')
   res.end(data)
 }
