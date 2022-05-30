a96447e652d0bcaf73b24743a7de83b9a8a5f51b
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-05-30 10:03:54 -0500

    :recycle: refactor: add more icons and update router lib

--- src/shared/utils/router.ts
+++ src/shared/utils/router.ts
@@ -1,6 +1,6 @@
 export const getAbsoluteURL = (path: string) => {
   const baseURL = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000'
+    : 'http://localhost:3003'
   return baseURL + path
 }
